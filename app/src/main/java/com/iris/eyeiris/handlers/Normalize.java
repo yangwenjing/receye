0c77ff7cd641169b023ba873b06f7131a666f501
Wenjing Yang <yangwenjing@buaa.edu.cn>
2015-10-23 17:30:17 +0800

    data

---
+++ app/src/main/java/com/iris/eyeiris/handlers/Normalize.java
@@ -0,0 +1,13 @@
+package com.iris.eyeiris.handlers;
+
+import org.opencv.core.Mat;
+
+/**
+ * Created by ywj on 15/10/23.
+ */
+public class Normalize {
+    public static Mat normalize(Mat src, Mat circles) {
+        //争强和归一化
+        return null;
+    }
+}
