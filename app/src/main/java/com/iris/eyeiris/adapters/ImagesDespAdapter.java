01a03b94b89be4ac35754d0861ff7f68964cffdb
Wenjing Yang <yangwenjing@buaa.edu.cn>
2015-11-02 09:43:23 +0800

    #ImageDes Adapter

---
+++ app/src/main/java/com/iris/eyeiris/adapters/ImagesDespAdapter.java
@@ -0,0 +1,7 @@
+package com.iris.eyeiris.adapters;
+
+/**
+ * Created by ywj on 15/11/2.
+ */
+public class ImagesDespAdapter {
+}
---
+++ app/src/main/java/com/iris/eyeiris/entities/ImageDesp.java
@@ -0,0 +1,7 @@
+package com.iris.eyeiris.entities;
+
+/**
+ * Created by ywj on 15/11/2.
+ */
+public class ImageDesp {
+}
